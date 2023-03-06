61bee316424056ce256be1d8eea1d4090c7e927e
petrov6525 <petrov6525@gmail.com>
2023-03-06 21:45:20 +0200

    0603232145

---
+++ Azure_DZ1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure;
+
+
+string conString = "DefaultEndpointsProtocol=https;AccountName=daryncevstudent;AccountKey=Di2rFiceS4dsU8ghZkk18EcY/CacChk4QLK8keOgLyLQH8S+XuG/6ngZvEFLarD0fYfM33Pvq+0i+ASto3vfTg==;EndpointSuffix=core.windows.net";
+
+var blobServiceClient = new BlobServiceClient(conString);
+
+string containerName = "conn1b54ff75-36b9-405c-943e-bbba7cb643aa";
+
+
+string fileName = "Test.txt";
+
+
+BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+
+
+BlobClient blobClient = blobContainerClient.GetBlobClient(fileName);
+
+
+Response<BlobDownloadInfo> downloadResponse = await blobClient.DownloadAsync();
+BlobDownloadInfo downloadInfo = downloadResponse.Value;
+
+string contents;
+
+using (var reader = new StreamReader(downloadInfo.Content))
+{
+    contents = await reader.ReadToEndAsync();
+}
+Console.WriteLine("File contents: " + contents);
+
+
+Console.ReadLine();
+
+
